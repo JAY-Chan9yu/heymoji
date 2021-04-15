2f1ea34698a5f223ff9a4df141c22f0ead0d2f21
jay <ckj9014@gmail.com>
2021-04-15 16:44:06 +0900

    [scripts] 필드네임 오타 수정

--- scripts/update_emoji_count.py
+++ scripts/update_emoji_count.py
@@ -10,7 +10,7 @@ connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DATABAS
 
 with connection:
     with connection.cursor() as cursor:
-        sql = "UPDATE slack_user SET get_emoji_count = 5"
+        sql = "UPDATE slack_user SET using_emoji_count = 5"
         cursor.execute(sql)
 
     connection.commit()
