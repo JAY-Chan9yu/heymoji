16d65ccf192c375d0ab660ffce777af1a44e4355
jay <ckj9014@gmail.com>
2021-04-17 16:20:28 +0900

    [scripts] 변경된 모델 필드에 맞게 쿼리 수정, 모듈 못찾는 오류가 있어서 DB 관련 정보들 하드코딩으로 넣어줌 (todo: 모듈 찾을 수 있도로 수정)

--- scripts/update_emoji_count.py
+++ scripts/update_emoji_count.py
@@ -1,14 +1,22 @@
 import pymysql
 
-from conf.config import Settings
+# TODO: 모듈을 제대로 불러오지 못하는 경우가 생김. root 디렉토리나, 모듈을 찾을 수 있게 해줘야 할 것 같은데.. 흠..
+# from conf.config import Settings
+# settings = Settings(_env_file='prod.env')
+# connection = pymysql.connect(host=settings.HOST, user=settings.USERNAME,
+#                              password=settings.PASSWORD, db=settings.DATABASE)
 
-settings = Settings(_env_file='prod.env')
-connection = pymysql.connect(host=settings.HOST, user=settings.USERNAME,
-                             password=settings.PASSWORD, db=settings.DATABASE)
+HOST = 'HOST'
+DATABASE = 'DATABASE'
+USERNAME = 'USERNAME'
+PASSWORD = 'PASSWORD'
+
+connection = pymysql.connect(host=HOST, user=USERNAME, password=PASSWORD, db=DATABASE)
+
 
 with connection:
     with connection.cursor() as cursor:
-        sql = "UPDATE slack_user SET my_reaction = 5"
+        sql = "UPDATE users SET my_reaction = 5"
         cursor.execute(sql)
 
     connection.commit()
