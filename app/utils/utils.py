7a47716ad0e9d1676142547ee887a61fe885acff
JayJi <ckj9014@gmail.com>
2022-01-31 12:31:42 +0900

    feat: cmd 맵핑시 avatar_url 값에서 <>(링크표시) 문자열 제거

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -5,8 +5,18 @@ def mapping_slack_command_to_dict(event_command: list) -> dict:
 
     for cmd in event_command:
         try:
-            info = cmd.split('=')
-            mapped_attr[info[0].replace('--', '')] = info[1]
+            # todo: 커맨드 형태 개선하기
+            if '--avatar_url' in cmd:
+                info = cmd.split('--avatar_url=')
+                key = 'avatar_url'
+                value = info[1].strip('<>')
+            else:
+                info = cmd.split('=')
+                key = info[0].replace('--', '')
+                value = info[1]
+
+            mapped_attr[key] = value
+
         except:
             continue
 
