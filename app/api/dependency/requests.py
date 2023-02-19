59508b6176732b3329a2002e6da32affcb47dfb5
지찬규 <ckj9014@gmail.com>
2023-02-20 02:45:26 +0900

    feat: get_slack_event 메서드 코드 정리

--- app/api/dependency/requests.py
+++ app/api/dependency/requests.py
@@ -6,19 +6,30 @@ from app.applications.schemas import SlackChallengeHook, SlackMentionHook, Slack
 from app.applications.services.slack_services import SLACK_EVENT_HOOKS
 from app.domains.reactions.entities import SlackEventType
 
+
 async def get_slack_event(request: Request) -> SLACK_EVENT_HOOKS:
-    
-    request_data: dict
     try:
-        request_data = await request.json()
+        request_data: dict = await request.json()
+        challenge_request = request_data.get('challenge')
+        event_request = request_data.get('event')
     except JSONDecodeError:
         raise HTTPException(status_code=400, detail="request body can not be none")
-    
-    if request_data.get('challenge'):
+
+    if challenge_request:
         return SlackChallengeHook(**request_data)
-    elif request_data['event'].get('bot_profile'):
-        return SlackBotDirectMessageHook(**request_data)
-    elif request_data['event']['type'] in [SlackEventType.APP_MENTION_REACTION.value, SlackEventType.APP_MESSAGE.value]:
-        return SlackMentionHook(**request_data)
+    elif event_request:
+        if event_request.get('bot_profile'):
+            return SlackBotDirectMessageHook(**request_data)
+        elif is_slack_mention(event_request['type']):
+            return SlackMentionHook(**request_data)
+        else:
+            return SlackEventHook(**request_data)
     else:
-        return SlackEventHook(**request_data)
+        raise HTTPException(status_code=403, detail="please check slack event")
+
+
+def is_slack_mention(event_type: str) -> bool:
+    return event_type in [
+        SlackEventType.APP_MENTION_REACTION.value,
+        SlackEventType.APP_MESSAGE.value
+    ]
